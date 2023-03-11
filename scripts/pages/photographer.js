9fa3629d04f51811555269ca3111d59073bc2e99
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-03-11 14:48:26 +0100

    photographer header section done

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -4,24 +4,31 @@ async function getPhotographer() {
         const response =  await fetch("../data/photographers.json")
         const datas = await response.json()
         console.log(datas)
-        const params = URL.searchParams
-        const id = params.get('id')
-        console.log('id:',id)
-        //datas.filter()
-        //return {photographer: datas.photographers}
+        const params = (new URL(document.location)).searchParams
+        const id = parseInt(params.get('id'))
+        const photographers = datas.photographers
+        const photographer = photographers.filter(photographer => photographer.id === id) // gerer si photographe inexistant ou si plusieurs photographes avec mm id
+        console.log(photographer)
+        return photographer[0]
     }
     catch(error){
         console.error(error)
     }
 }
 
+async function displayPhotographer(photographer) {
+    const photographersSection = document.querySelector(".photograph-header");
 
+    const photographerModel = photographerFactory(new Photographer(photographer));
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+}
 
 
 async function init() {
     // Récupère les datas des photographes
-    const { photographers } = await getPhotographer();
-    if(photographers) displayData(photographers); // doit display une error dans le cas contraire ou plutot dans le catch?
+    const photographer = await getPhotographer();
+    if(photographer) displayPhotographer(photographer); // doit display une error dans le cas contraire ou plutot dans le catch?
 };
 
 init();
