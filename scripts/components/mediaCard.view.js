7464110f5fabc47e222fe3a3ad74c667230d6d6c
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-03-14 19:12:05 +0100

    component implementation

--- scripts/components/mediaCard.view.js
+++ scripts/components/mediaCard.view.js
@@ -1,5 +1,5 @@
 
-const imgGalleryCardComponent = ({src, title, likes}) => {
+const imgGalleryCardView = ({src, title, likes}) => {
     const DOMStringified = 
     `<article>
         <img src='${src}'>
@@ -8,4 +8,39 @@ const imgGalleryCardComponent = ({src, title, likes}) => {
             <p>>${likes}</p>
         </div>
     </article>`
+
+    return {DOMStringified, buildParams : ['src', 'title', 'likes']}
+
+    /*listBuildingParams(){
+        return ['src', 'title', 'likes']
+    }*/
 }
+
+class Component {
+    #view
+    #params
+    constructor(view, buildingParams){ //buildingParams ou buildingParamsList?
+        this.#view = view
+        this.#params = buildingParams
+        // DOM parser(view(buildingParams)
+    }
+
+    setParams(buildingParams){
+        this.#params = buildingParams
+    }
+
+    render(){
+        return this.#view(this.#params)
+    }
+
+    getParams(){
+        return this.#params
+    }
+}
+
+/*
+
+return objet {param1 : vide, param2 : vide, param3: vide}
+pr le rebuild il faut pouvoir lui passer de nouveaux params et dc il faut avoir conserve la view
+
+*/
